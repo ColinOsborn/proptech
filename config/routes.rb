c03fcc5798fb7518b56d8bdd3b1a7ccbd2014fbd
Colin Osborn <colin.osborn@gmail.com>
2025-04-16 20:51:46 -0600

    added correct routes to handle login and signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: "registrations" }
+
+  root "home#index"
+
+  devise_scope :user do
+    get "login", to: "devise/sessions#new"
+    get "signup", to: "devise/registrations#new"
+    get "/users/sign_out" => "devise/sessions#destroy"
+  end
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   get "up" => "rails/health#show", as: :rails_health_check
-
-  root "home#index"
 end
